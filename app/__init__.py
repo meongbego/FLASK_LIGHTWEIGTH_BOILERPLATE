fe33a241ecd54d1b2377b1545d32e1b21ce97760
Sofyan Saputra <meongbego@gmail.com>
2019-02-21 05:55:06 +0700

    First Init

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from . import configs
+from flask import Flask
+import psycopg2
+
+app = Flask(__name__)
+app.config.from_object(configs.Config)
+
+
+conn = psycopg2.connect(
+    database=os.getenv('DB_NAME'),
+    user=os.getenv('DB_USER'),
+    sslmode=os.getenv('DB_SSL'),
+    port=os.getenv('DB_PORT'),
+    host=os.getenv('DB_HOST')
+)
+
+conn.set_session(autocommit=True)
+db = conn.cursor()
+
+# registering controllers
+from app.controllers import *
+
+
